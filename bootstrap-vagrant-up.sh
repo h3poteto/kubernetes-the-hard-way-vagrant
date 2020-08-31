c2ba99a845f0ed14adcafb5db5d7d7adf7daae89
AkiraFukushima <h3.poteto@gmail.com>
2020-08-31 19:24:39 +0900

    Configura vagrant up script to root systemctl

--- bootstrap-vagrant-up.sh
+++ bootstrap-vagrant-up.sh
@@ -2,18 +2,20 @@
 
 CURRENT_PATH=$(pwd)
 
-cat <<EOF | tee ~/.config/systemd/user/vagrant.service
+cat <<EOF | sudo tee /etc/systemd/system/vagrant-up.service
 [Unit]
 Description=vagrant up
 After=network.target
 ConditionPathExists=${CURRENT_PATH}
 
 [Service]
+User=h3poteto
+Group=h3poteto
 Type=forking
 ExecStart=${CURRENT_PATH}/vagrant-up.sh
 Restart=no
-TimeoutSec=10min
+TimeoutSec=15min
 
 [Install]
-WantedBy=default.target
+WantedBy=multi-user.target
 EOF
